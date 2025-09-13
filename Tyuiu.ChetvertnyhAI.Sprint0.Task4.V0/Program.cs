d0ceec7a7ef7747d02ac06cd36d0c93608e22ee6
ChetvertnyhArina <chetvertnyh-arina@mail.ru>
2025-09-14 01:00:05 +0500

    Task4

---
+++ Tyuiu.ChetvertnyhAI.Sprint0.Task4.V0/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Tyuiu.ChetvertnyhAI.Sprint0.Task4.V0.Lib;
+namespace Tyuiu.ChetvertnyhAI.Sprint0.Task4.V0
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine(DataService.Addition(17,6));
+            Console.WriteLine(DataService.Subtraction(143, 59));
+            Console.WriteLine(DataService.Multiplication(18, 5));
+            Console.WriteLine(DataService.Division(7, 7));
+            Console.ReadKey();
+        }
+    }
+}
