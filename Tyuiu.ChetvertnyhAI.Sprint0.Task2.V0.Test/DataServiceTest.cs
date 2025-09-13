60ab68fcfc1b34bc109c78b117480bdd77a0e501
ChetvertnyhArina <chetvertnyh-arina@mail.ru>
2025-09-13 23:41:26 +0500

    Task2

---
+++ Tyuiu.ChetvertnyhAI.Sprint0.Task2.V0.Test/DataServiceTest.cs
@@ -0,0 +1,16 @@
+﻿using Tyuiu.ChetvertnyhAI.Sprint0.Task2.V0.Lib;
+namespace Tyuiu.ChetvertnyhAI.Sprint0.Task2.V0.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckGetMessageValid()
+        {
+            var name = "Арина";
+            var res = DataService.GetMessage(name);
+
+            Assert.AreEqual("Привет.., Арина", res);
+        }
+    }
+}
---
+++ Tyuiu.ChetvertnyhAI.Sprint0.Task2.V0/Program.cs
@@ -0,0 +1,13 @@
+﻿using Tyuiu.ChetvertnyhAI.Sprint0.Task2.V0.Lib;
+namespace Tyuiu.ChetvertnyhAI.Sprint0.Task2.V0
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine(DataService.GetMessage("Арина"));
+            Console.ReadKey();
+            Console.ReadKey();
+        }
+    }
+}
