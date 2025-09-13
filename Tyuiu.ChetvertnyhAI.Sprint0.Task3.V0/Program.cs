5e9b9f6b5b7d643fe9a2e04e970f690c933a9afe
ChetvertnyhArina <chetvertnyh-arina@mail.ru>
2025-09-14 00:18:23 +0500

    Task3

---
+++ Tyuiu.ChetvertnyhAI.Sprint0.Task3.V0/Program.cs
@@ -0,0 +1,12 @@
+﻿using Tyuiu.ChetvertnyhAI.Sprint0.Task3.V0.Lib;
+namespace Tyuiu.ChetvertnyhAI.Sprint0.Task3.V0
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine(DataService.Sum(97, 58));
+            Console.ReadKey();
+        }
+    }
+}
